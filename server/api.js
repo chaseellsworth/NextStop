019832a21c1c792d95177744c8587a54758a3dda
Chase Ellsworth <chaseme3@gmail.com>
2015-02-09 15:00:49 -0800

    sets up databases 10

--- server/api.js
+++ server/api.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//INSERT THE ROUTERS
+
 // var projectRouter = require('./project/projectRoutes');
 // var userRouter = require('./user/userRoutes');
 // var fileRouter = require('./file/fileRouter');
