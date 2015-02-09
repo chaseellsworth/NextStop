4664e97d8de85d7612ab6bf6719bda028fdf78bf
Chase Ellsworth <chaseme3@gmail.com>
2015-02-09 15:48:12 -0800

    sets up skeleton for auth with passport 2

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -1,22 +1,62 @@
 'use strict';
 
-//Dependencies
-var config = require('config');
-var Q = require('q');
-var _ = require('lodash');
-
-// Models
-var models = require('../models.js').models;
-var db = require('../db');
-
-// Required Controllers
-
-// Utility Functions
-
+//Internal dependencies
 var authController = {};
+var UserCollection = require('../models').collections.UserCollection;
+var User = require('../models').models.User;
 
-/**
- * USE FOR EXPLANATIONS
- */
+authController.getUser = function (req, res) {
+	var userId = null;
+	var userName = null;
+	var email = null;
+	var githubAvatarUrl = 'http://www.gravatar.com/avatar/00000000000000000000000000000000?d=mm&f=y';
+	if (req.user && req.user.get('id') && typeof req.user.get('id') === 'number') {
+		userId = req.user.get('id');
+		email = req.user.get('email');
+		userName = req.user.get('username') || email;
+		githubAvatarUrl = req.user.get('githubAvatarUrl') || githubAvatarUrl;
+	}
+	res.json({
+		userId: userId,
+		// userName: req.user.get('username'),
+		userName: userName,
+		email: email,
+		githubAvatarUrl: githubAvatarUrl
+	});
+};
+
+authController.logout = function (req, res) {
+	req.logout();
+	res.redirect('/');
+};
+
+authController.login = function (req, res) {
+	res.redirect('/#/home');
+};
+
+authController.signup = function (req, res) {
+	var email = req.body.email || req.param('email');
+	var password = req.body.password || req.param('password');
+	if (!email || !password) {
+		res.status(400).end(); // Client Error
+		return;
+	}
+	new UserCollection()
+		.query('where', 'email', '=', email)
+		.fetchOne()
+		.then(function (user) {
+			if (user !== null) {
+				res.redirect('/#/home');
+				return;
+			}
+			new User({
+					email: email,
+					password: password
+				}).save()
+				.then(function () {
+					res.redirect('/#/home');
+				});
+		});
+};
 
 module.exports = authController;
