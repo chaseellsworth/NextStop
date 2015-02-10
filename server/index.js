169d2e572ea0cfd934bb3c4fcf4a38d29690f0df
Chase Ellsworth <chaseme3@gmail.com>
2015-02-09 16:14:59 -0800

    sets up skeleton for auth with passport 5

--- server/index.js
+++ server/index.js
@@ -1,26 +1,22 @@
 'use strict';
 
+//External dependencies
 var config = require('config');
-
 console.log('config:', config);
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var express = require('express');
 
-//dependencies
-var bodyParser = require('body-parser'),
-  session = require('express-session'),
-  express = require('express'),
-  chatServer = require('./chatServer.js'),
-  shareJSServer = require('./sharejs/shareJSServer');
-
-// Set routes
+//Internal dependencies
 var clientConfigParser = require('./clientConfigParser');
 var auth = require('./auth');
 var authRouter = require('./auth/authRouter');
 var apiRouter = require('./api');
 
-// Init app
+//Init app
 var app = express();
 
-// Middlewares
+//Middlewares
 app
   .use(bodyParser.urlencoded({
     extended: true
@@ -34,7 +30,7 @@ app
   .use(auth.initialize())
   .use(auth.session());
 
-// Set Routes
+//Set Routes
 app
   .use('/config.js', clientConfigParser)
   .use(express.static(__dirname + '/../client'))
@@ -44,9 +40,5 @@ app
     console.log('Server listening on port:', config.get('ports').http);
   });
 
-chatServer.listen(config.get('ports').chat);
-shareJSServer.listen(config.get('ports').editor);
-console.log('Chat listening on port:', config.get('ports').chat);
-console.log('Editor listening on port:', config.get('ports').editor);
-
+//Export
 module.exports = app;
